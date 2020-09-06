ce4048992e1905c681d2214f91b1e339c4a309ec
quahjieren@gmail.com <quahjieren@gmail.com>
2020-09-06 17:49:29 +0800

    Added gitignore, removed csv files

--- export-process-info/export_process_input.ps1
+++ export-process-info/export_process_input.ps1
@@ -1,17 +1,27 @@
-###
-# Exports information about the processes running on the current machine and exports them into a CSV file
-# Takes user input
-# Requirements
-# 1) name, process ID, number of CPUs and number of handles
-# 2) Handles determined by user input, organized in ascending order
-###
+<#
+.SYNOPSIS
+Exports information about the processes running on the current machine and exports them into a CSV file
+.DESCRIPTION
+Outputs name, process ID, number of CPUs and number of handles
+Handles determined by user input, organized in ascending order
+.PARAMETER handle
+Minimum number of handles required
+#>
 
-# Get user input for handles
-$handle_input = Read-Host 'Please enter minimum number of handles.'
-# $get_pw = Read-Host 'Please enter password (just for fun)' -AsSecureString
+# Export processes by prompting for user input for handles
+# $handle_input = Read-Host 'Please enter minimum number of handles.'
 
-# Export process
-# Don't append since we only write once
+# Get-Process | `
+# Select-Object Name, Id, @{name='CPU'; expression={$_.TotalProcessorTime}}, Handles | `
+# Where-Object Handles -ge $handle_input | `
+# Sort-Object -property Handles | `
+# Select-Object Name, Id, CPU, Handles | `
+# Export-Csv -Path result_input.csv -NoTypeInformation
+
+# Export processes by prompting for user input via parameter
+Param(
+    [Parameter(Mandatory=$true)][string]$handle_input
+)
 Get-Process | `
 Select-Object Name, Id, @{name='CPU'; expression={$_.TotalProcessorTime}}, Handles | `
 Where-Object Handles -ge $handle_input | `
